542dad27ea2b43ff8bc3ea26508a5bf45968a1d5
KenSporger <ken.sporger@outlook.com>
2020-08-25 03:56:52 -0700

    add sample.cpp

---
+++ utilities/Inc/sample.h
@@ -0,0 +1,25 @@
+#ifndef _SAMPLE_H
+#define _SAMPLE_H
+
+#include<iostream>
+#include<vector>
+#include<fstream>
+#include<ctime>
+
+using namespace std;
+
+
+class SampleWriter
+{
+    public:
+        SampleWriter(std::vector<std::pair<std::string, double*>> vars):
+            _vars(vars){}
+        int update();
+        void write();
+    private:
+        std::vector<std::pair<std::string, double*>> _vars;
+        std::vector<vector<double>> histroy;
+};
+
+
+#endif
